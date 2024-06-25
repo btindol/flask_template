bbaf3744b8da36bd72afedab4851b91bb963c426
btindol <blake.tindol@stryker.com>
2024-06-25 08:14:46 -0400

    simple dockerized flask

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,20 @@
-# syntax=docker/dockerfile:1
+# Use the official Python image from the Docker Hub
+FROM python:3.9
 
-FROM python:3.11
+# Set the working directory
+WORKDIR /app
 
-WORKDIR /code
+# Copy the current directory contents into the container at /app
+COPY . /app
 
-COPY requirements.txt .
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
-RUN pip install -r requirements.txt
+# Make port 8080 available to the world outside this container
+EXPOSE 8080
 
-COPY . .
+# Define environment variable
+ENV FLASK_APP=app.py
 
-EXPOSE 5000
-
-ENTRYPOINT ["gunicorn", "app:app"]
+# Run app.py when the container launches
+CMD ["flask", "run", "--host=0.0.0.0", "--port=8080"]
--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+class Album:
+    def __init__(self, id, title, artist, price, image_url):
+        self.id = id
+        self.title = title
+        self.artist = artist
+        self.price = price
+        self.image_url = image_url
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+albums = [
+    Album(1, "You, Me and an App Id", "Daprize", 10.99, "https://aka.ms/albums-daprlogo"),
+    Album(2, "Seven Revision Army", "The Blue-Green Stripes", 13.99, "https://aka.ms/albums-containerappslogo"),
+    Album(3, "Scale It Up", "KEDA Club", 13.99, "https://aka.ms/albums-kedalogo"),
+    Album(4, "Lost in Translation", "MegaDNS", 12.99, "https://aka.ms/albums-envoylogo"),
+    Album(5, "Lock Down Your Love", "V is for VNET", 12.99, "https://aka.ms/albums-vnetlogo"),
+    Album(6, "Sweet Container O' Mine", "Guns N Probeses", 14.99, "https://aka.ms/albums-containerappslogo")
+]
+
+@app.route("/")
+def read_root():
+    return jsonify({"message": "Access /albums to see the list of albums"})
+
+@app.route("/albums")
+def get_albums():
+    albums_list = [{
+        "id": album.id,
+        "title": album.title,
+        "artist": album.artist,
+        "price": album.price,
+        "image_url": album.image_url
+    } for album in albums]
+    return jsonify(albums_list)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
